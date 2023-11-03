2c634367bc34f8792233ba63c5074eba655e8734
tatarinovst2 <SpaceUltraMax@yandex.ru>
2023-11-03 06:17:37 +0300

    Test fixes

--- config/run_coverage.sh
+++ config/run_coverage.sh
@@ -10,6 +10,11 @@ configure_script
 
 directories=$(get_project_directories)
 
+if [[ -z "${directories}" ]]; then
+  echo "No files to run coverage on currently."
+  exit 0
+fi
+
 coverage run -m pytest "${directories}"
 coverage report
 
--- config/run_mypy.sh
+++ config/run_mypy.sh
@@ -10,8 +10,14 @@ configure_script
 
 directories=$(get_project_directories)
 
-python3 -m mypy "${directories}"
+python3 -m mypy config
 
 check_if_failed
+
+for directory in $directories; do
+  python3 -m mypy "${directory}"
+
+  check_if_failed
+done
 
 echo "Mypy check passed."
